18a7f8ee9713fd648335b2e54904e75d9c9a3bae
Raphaël Colin <raphael.colin68@gmail.com>
2021-02-26 23:24:59 +0100

    Update exercices wernick
    Wernick 13 -> Fonctionne
    Wernick 14 et 15 -> Problème étrange ?

---
+++ Wernick/w14.pl
@@ -0,0 +1,12 @@
+0 'dec:' point :: a donne.
+1 'dec:' point :: o donne.
+2 'dec:' point :: ha donne.
+3 'dec:' point :: b cherche.
+4 'dec:' point :: c cherche.
+
+0 'cont:' o '=p=' ccc(a, b, c).
+1 'cont:' ha '=p=' prj(a, dro(b, c)).
+
+0 'cnd:' a diff b.
+1 'cnd:' b diff c.
+2 'cnd:' a diff c.
