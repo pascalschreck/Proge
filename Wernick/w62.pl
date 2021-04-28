8197668211ce088951264c5853f48877011c9335
Raphaël Colin <raphael.colin68@gmail.com>
2021-04-29 01:53:31 +0200

    Exercices 62 et 63

---
+++ Wernick/w62.pl
@@ -0,0 +1,14 @@
+0 'dec:' point :: o donne.
+1 'dec:' point :: ma donne.
+2 'dec:' point :: mb donne.
+3 'dec:' point :: a cherche.
+4 'dec:' point :: b cherche.
+5 'dec:' point :: c cherche.
+
+0 'cont:' o '=p=' ccc(a, b, c).
+1 'cont:' ma '=p=' mil(b, c).
+2 'cont:' mb '=p=' mil(a, c).
+
+0 'cnd:' a diff b.
+1 'cnd:' b diff c.
+2 'cnd:' a diff c.
